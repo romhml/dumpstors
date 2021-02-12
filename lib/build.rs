fe516d1856356c203f961e40c8ab55083a0d3c9e
Romain Hamel <romain.hamel@untienots.com>
2021-02-12 18:14:31 +0100

    Debugging grcov

--- lib/build.rs
+++ lib/build.rs
@@ -1,11 +1,19 @@
 use std::error::Error;
 use std::process::exit;
 
-fn main() {
+fn run() -> Result<(), Box<dyn Error>> {
     tonic_build::configure()
         .format(false) // disable code formatting since docs.rs will otherwise break
-        .compile(&["proto/models.proto", "proto/store.proto"], &["."]).unwrap();
+        .compile(&["proto/models.proto", "proto/store.proto"], &["."])?;
+    Ok(())
+}
 
-    println!("cargo:rerun-if-changed=proto/models.proto");
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("{}", err);
+        exit(1);
+    }
+
     println!("cargo:rerun-if-changed=proto/store.proto");
+    println!("cargo:rerun-if-changed=proto/models.proto");
 }
