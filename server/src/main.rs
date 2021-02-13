87f08bf5bf133617c2603f7729a1be343f524147
Romain Hamel <romain.hamel@untienots.com>
2021-02-13 11:55:32 +0100

    Setup logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,7 @@ extern crate serde_derive;
 
 use std::sync::{Arc, Mutex};
 use tonic::transport::Server;
+use log::*;
 
 use dumpstors_lib::store::store_server::StoreServer;
 use dumpstors_lib::store::Store;
@@ -13,11 +14,16 @@ mod store;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
     let conf = settings::Settings::new().unwrap();
     let sockaddr = format!("{}:{}", conf.listen_addr, conf.port);
 
+    info!("Loading store at '{}'", conf.store.path);
     let store = Arc::new(Mutex::new(Store::new(conf.store.path)));
     let store_srv = store::DumpstorsStoreServer::new(store.clone());
+
+    info!("Starting server on '{}'", sockaddr);
 
     Server::builder()
         .add_service(StoreServer::new(store_srv))
